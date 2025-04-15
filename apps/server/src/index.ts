42b7cbe5c927067d0ad1b08541cda3bb39849655
Brandon Ly <brandonly@lostcoding.com>
2025-04-15 11:53:46 -0500

    Add node server

---
+++ apps/server/src/index.ts
@@ -0,0 +1,17 @@
+import Fastify from "fastify";
+
+const fastify = Fastify({
+	logger: true,
+});
+// Declare a route
+fastify.get("/", (_, reply) => {
+	reply.send({ hello: "world" });
+});
+
+// Run the server!
+fastify.listen({ port: 3000 }, (err, _) => {
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+});
