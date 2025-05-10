1b4d1463dc57f5291809ea7e7419f55e752f203d
Brandon Ly <brandonly@lostcoding.com>
2025-05-10 16:47:13 -0500

    copy gmail code example

--- apps/cloud/src/index.ts
+++ apps/cloud/src/index.ts
@@ -5,13 +5,6 @@ import type {
 } from "aws-lambda";
 import path from "node:path";
 
-const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = path.join(process.cwd(), "token.json");
-const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
-
 /**
  * Core authentication handler. This function is called by the frontend after a user is authenticated by GitHub.
  * TODO: For local development, we'll want the ability to allow developers to provide their own GitHub personal access token.
--- apps/core/src/gmail/index.ts
+++ apps/core/src/gmail/index.ts
@@ -1,3 +1,15 @@
+import { google } from "googleapis";
+import path from "node:path";
+import fs from "node:fs/promises";
+import { authenticate } from "@google-cloud/local-auth";
+
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), "token.json");
+const CREDENTIALS_PATH = path.join(process.cwd(), "credentials.json");
+
 /**
  * Reads previously authorized credentials from the save file.
  *
@@ -67,7 +79,8 @@ async function listLabels(auth) {
 		return;
 	}
 	console.log("Labels:");
-	labels.forEach((label) => {
+
+	for (const label of labels) {
 		console.log(`- ${label.name}`);
-	});
+	}
 }
